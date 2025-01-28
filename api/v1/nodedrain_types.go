c856775d24d1e15f8a4aea48d9eccba6c1fb8829
Gerald Barker <gezb@users.noreply.github.com>
2025-01-28 21:31:43 +0000

    Adjust CRDs

--- api/v1/nodedrain_types.go
+++ api/v1/nodedrain_types.go
@@ -4,27 +4,49 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+type NodeDrainPhase string
+
+const (
+	// NodeDrainFinalizer is a finalizer for a NodeMaintenance CR deletion
+	NodeDrainFinalizer string = "co.uk.gezb,NodeDrain"
+
+	NodeDrainPhasePending   NodeDrainPhase = "Pending"
+	NodeDrainPhaseCordoned  NodeDrainPhase = "Cordoned"
+	NodeDrainPhaseDraining  NodeDrainPhase = "Draining"
+	NodeDrainPhaseCompleted NodeDrainPhase = "Completed"
+	NodeDrainPhaseFailed    NodeDrainPhase = "Failed"
+)
 
 // NodeDrainSpec defines the desired state of NodeDrain.
 type NodeDrainSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of NodeDrain. Edit nodedrain_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	NodeName string `json:"nodeName"`
 }
 
 // NodeDrainStatus defines the observed state of NodeDrain.
 type NodeDrainStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Phase represents the progress of this nodeDrain
+	Phase NodeDrainPhase `json:"phase"`
+	// The last time the status has been updated
+	LastUpdate metav1.Time `json:"lastUpdate,omitempty"`
+	// LastError represents the latest error if any in the latest reconciliation
+	LastError string `json:"lastError,omitempty"`
+	// PendingPods is a list of pending pods for eviction
+	PendingPods []string `json:"pendingpods,omitempty"`
+	// TotalPods is the total number of all pods on the node from the start
+	// +operator-sdk:csv:customresourcedefinitions:type=status
+	TotalPods int `json:"totalpods,omitempty"`
+	// EvictionPods is the total number of pods up for eviction from the start
+	EvictionPodCount int `json:"evictionPods,omitempty"`
+	// Percentage completion of draining the node
+	DrainProgress int `json:"drainProgress,omitempty"`
+	// PodsBlockingDrain is a list of pods that are blocking the draining of this node
+	PodsBlockingDrain string `json:"podsblockingdrain,omitempty"`
 }
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
-// +kubebuilder:resource:scope=Cluster
+// +kubebuilder:printcolumn:name="Phase",type="string",JSONPath=".status.phase",description="Phase of the NodeDrain"
+// +kubebuilder:printcolumn:name="Pods BlockingDrain",type="string",JSONPath=".status.podsblockingdrain",description="Pods that are blocking drain"
 
 // NodeDrain is the Schema for the nodedrains API.
 type NodeDrain struct {
